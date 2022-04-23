a43c69dd7b554c8e72383432d4ef389d0bd5baa6
Hassan Masood <hassanmasood2002@gmail.com>
2022-04-24 00:23:52 +0500

    Added New Product

--- src/pages/newProduct/NewProduct.jsx
+++ src/pages/newProduct/NewProduct.jsx
@@ -1,6 +1,45 @@
+import { useState } from "react";
 import "./newProduct.css";
 
 export default function NewProduct() {
+  const [title, setTitle] = useState("");
+  const [price, setPrice] = useState(0);
+  const [description, setDescription] = useState("");
+  const [categories, setCategories] = useState([]);
+  const [size, setSize] = useState([]);
+  const [color, setColor] = useState([]);
+  const [stock, setStock] = useState(false);
+  const [file, setFile] = useState(null);
+
+  const fileHandler = () => {};
+
+  const titleHandler = (event) => {
+    setTitle(event.target.value);
+  };
+  const priceHandler = (event) => {
+    setPrice(event.target.value);
+  };
+  const sizeHandler = (event) => {
+    setSize(event.target.value.split(","));
+  };
+  const categoryHandler = (event) => {
+    setCategories(event.target.value.split(","));
+  };
+  const colorHandler = (event) => {
+    setColor(event.target.value.split(","));
+  };
+  const descriptionHandler = (event) => {
+    setDescription(event.target.value);
+  };
+  const stockHandler = (event) => {
+    setStock(event.target.value);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+  };
+
+  console.log(size, color, categories, title, description, stock, price);
   return (
     <div className="newProduct">
       <h1 className="addProductTitle">New Product</h1>
@@ -10,21 +49,57 @@ export default function NewProduct() {
           <input type="file" id="file" />
         </div>
         <div className="addProductItem">
-          <label>Name</label>
-          <input type="text" placeholder="Apple Airpods" />
+          <label>Title</label>
+          <input
+            type="text"
+            placeholder="Apple Airpods"
+            onChange={titleHandler}
+          />
+        </div>
+        <div className="addProductItem">
+          <label>Price</label>
+          <input type="Number" placeholder="0.0$" onChange={priceHandler} />
+        </div>
+
+        <div className="addProductItem">
+          <label>Categories</label>
+          <input
+            type="text"
+            placeholder="jeans,skirts"
+            onChange={categoryHandler}
+          />
+        </div>
+        <div className="addProductItem">
+          <label>Size</label>
+          <input type="text" placeholder="XL,X,M,S" onChange={sizeHandler} />
+        </div>
+        <div className="addProductItem">
+          <label>Color</label>
+          <input
+            type="text"
+            placeholder="Yellow,Black,White"
+            onChange={colorHandler}
+          />
         </div>
         <div className="addProductItem">
           <label>Stock</label>
-          <input type="text" placeholder="123" />
-        </div>
-        <div className="addProductItem">
-          <label>Active</label>
-          <select name="active" id="active">
-            <option value="yes">Yes</option>
-            <option value="no">No</option>
+          <select onChange={stockHandler}>
+            <option value="true">True</option>
+            <option value="false">False</option>
           </select>
         </div>
-        <button className="addProductButton">Create</button>
+        <div className="addProductItem">
+          <label>Description</label>
+          <textarea
+            id="description"
+            onChange={descriptionHandler}
+            required
+            rows="5"
+          ></textarea>
+        </div>
+        <button className="addProductButton" onClick={submitHandler}>
+          Create
+        </button>
       </form>
     </div>
   );
