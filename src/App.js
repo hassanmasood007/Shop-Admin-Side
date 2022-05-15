cfa510d73528f09963e1c477236745cd385db354
Hassan Masood <hassanmasood2002@gmail.com>
2022-05-16 00:09:32 +0500

    Refactored

--- src/App.js
+++ src/App.js
@@ -12,9 +12,10 @@ import NewProduct from "./pages/newProduct/NewProduct";
 import Login from "./pages/login/Login";
 
 function App() {
-  const admin = JSON.parse(
-    JSON.parse(localStorage.getItem("persist:root")).user
-  ).currentUser.accessToken;
+  const admin = JSON.parse(localStorage.getItem("persist:root"))
+    ? JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user)
+        .currentUser.accessToken
+    : "";
   return (
     <Router>
       <Switch>
--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,23 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+const dotenv = require("dotenv");
+dotenv.config();
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBpHHeMPUcbVoTSrh4iA4tN3-M89K3alxc",
-  authDomain: "shop-e87d0.firebaseapp.com",
-  projectId: "shop-e87d0",
-  storageBucket: "shop-e87d0.appspot.com",
-  messagingSenderId: "449174708093",
-  appId: "1:449174708093:web:1855a63b0651f191df3614",
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGE_ID,
+  appId: process.env.APP_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 export default app;
+
+process.env.MONGO_URL;
